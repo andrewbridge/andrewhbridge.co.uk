1b6cc79d9302e18619324f79e2c3f2cbe2e6a0cd
Andrew Bridge <andrew@andrewhbridge.co.uk>
2023-01-21 16:24:46 +0000

    feat: Improve styles for Wordpress blog posts

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -6,8 +6,48 @@
 </script>
 
 <style>
-    .content :global(p), .content :global(li) {
+    .content :global(p), .content :global(li), .content :global(li *) {
         font-size: 18px;
+    }
+
+    .content :global(h2), .content :global(h3), .content :global(h4) {
+        margin: 1em 0 0.5em;
+    }
+
+    .content :global(.wp-block-image .aligncenter) {
+        text-align: center;
+    }
+
+    .content :global(.wp-block-image img) {
+        max-width: 100%;
+    }
+
+    .content :global([class*="wp-block-"]:not(.wp-block-heading)) {
+        margin-top: 16px;
+        margin-bottom: 16px;
+    }
+
+    .content :global(.wp-block-code code) {
+        width: 100%;
+        padding: 8px;
+        overflow-x: auto;
+    }
+
+    .content :global(.has-text-align-center) {
+        text-align: center;
+    }
+
+    .content :global(.has-small-font-size) {
+        font-size: 0.9em;
+    }
+
+    .content :global(p *) {
+        font-size: inherit;
+    }
+
+    .content :global(.wp-block-image .wp-element-caption) {
+        width: 75%;
+        margin: 0 auto;
     }
 </style>
 
